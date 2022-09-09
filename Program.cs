167871a8295db8bafbd62aaed337d8d1122c8ae2
никита клецко <nik.klec@mail.ru>
2022-09-09 16:11:01 +0300

    Добавление Решения задачи

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Методы 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
+
+//Задача
+string[] firstArray = { "ada", "asdas", "as", "asdas", "asd" };
+string[] lastArray = new string[firstArray.Length];
+int k = 0;
+Console.WriteLine("Имеющийся массив:");
+PrintArray(firstArray);
+for (int i = 0; i < firstArray.Length; i++)
+{
+    if (firstArray[i].Length <= 3)
+    {
+        lastArray[k] = firstArray[i];
+        k++;
+    }
+}
+Console.WriteLine("Полученный массив:");
+PrintArray(lastArray);
